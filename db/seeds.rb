71da45386a2cb8908ecb9f1334738cf9cf6a909b
Alina Kornilova <alinakornilova78@gmail.com>
2020-02-07 20:10:59 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+logic = Artist.create(name: "Logic")
+logic.songs.build(title: "Under Pressure").save
+logic.songs.build(title: "City Of Stars").save
+
+mac = Artist.create(name: "Mac Miller")
+mac.songs.build(title: "Woods").save
+mac.songs.build(title: "Circles").save 
